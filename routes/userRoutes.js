3e907865ff78745b0c50463302cda0d19158d9c5
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-05 16:40:37 +0530

    Added feature where users can upload and update their user/profile photos and also resized the image before saving

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,8 @@ const {
   updateMe,
   deleteMe,
   getMe,
+  uploadUserPhoto,
+  resizeUserPhoto,
 } = require('./../controllers/userController');
 
 const {
@@ -35,7 +37,7 @@ router.use(protect);
 router.patch('/updateMyPassword', updatePassword);
 
 router.get('/me', getMe, getUser);
-router.patch('/updateMe', updateMe);
+router.patch('/updateMe', uploadUserPhoto, resizeUserPhoto, updateMe);
 router.delete('/deleteMe', deleteMe);
 
 // Only Admins Can acces below routes
