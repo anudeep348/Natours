7735285258938c69614631c1d790395099a3ec50
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-06 21:41:01 +0530

    Added a service whre users can get email when they are signed up using Twilios sendGrid

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,73 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const { convert } = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) Create a Transporter
-  const Transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Anudeep Chowdary <${process.env.EMAIL_FROM}>`;
+  }
 
-  // 2) Define the email options
-  const mailOptions = {
-    from: 'Anudeep Chowdary <hello@anudeep.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
 
-  // 3) Actually send the email
-  await Transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Sending the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug templates
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+
+    // 2) Define Email Options
+    const mailOptions = {
+      // from: this.from,
+      from:
+        process.env.NODE_ENV === 'production'
+          ? process.env.SENDGRID_EMAIL_FROM
+          : this.form,
+      to: this.to,
+      subject,
+      html,
+      text: convert(html),
+    };
+
+    // 3) Create a transport and send Email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send(`welcome`, `Welcome to the Natours Family!`);
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      `Your Password Reset Token (Valid for 10 minutes)`,
+    );
+  }
 };
-
-module.exports = sendEmail;
