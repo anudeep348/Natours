8e0564de16bc876931b389d59009aec487031eb6
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-07 19:54:05 +0530

    fixed an issue where user gettig error while logging out of the application from a protected Route

--- public/js/login.js
+++ public/js/login.js
@@ -30,7 +30,12 @@ export const logout = async () => {
       url: 'http://127.0.0.1:3000/api/v1/users/logout',
     });
 
-    if (res.data.status === 'success') location.reload(true);
+    if (res.data.status === 'success') {
+      showAlert('success', `Logged out successfully!`);
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1000);
+    }
   } catch (err) {
     showAlert('error', 'Error loging out! Try again.');
   }
