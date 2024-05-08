33aae7099be321ddfbb28739f15d54e011755563
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-08 23:54:22 +0530

    Implimented a temporary way of creating a bookings when user paid sucess fully. Once we deploy we will impliment this functionality in full

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,10 +6,11 @@ const {
   getAccount,
 } = require('./../controllers/viewsController');
 const { protect, isLoggedIn } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.route('/').get(isLoggedIn, getOverview);
+router.route('/').get(createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
