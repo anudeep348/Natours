17ebf581637bed8184445ad3c30357ec81006312
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-05-09 16:01:26 +0530

    added a feature where loged in users can view their tour bookings

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,7 @@ const {
   getTour,
   getLoginForm,
   getAccount,
+  getMyTours,
 } = require('./../controllers/viewsController');
 const { protect, isLoggedIn } = require('../controllers/authController');
 const { createBookingCheckout } = require('../controllers/bookingController');
@@ -14,5 +15,6 @@ router.route('/').get(createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
+router.get('/my-tours', protect, getMyTours);
 
 module.exports = router;
